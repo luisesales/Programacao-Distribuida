ae58e96258dd20cecae27f5e10626c76f3a188fb
Luís Eduardo de Oliveira Sales <40070690+luisesales@users.noreply.github.com>
2025-05-26 16:40:38 +0000

    starting UDP Request Handlers

--- Kore/src/main/java/com/kore/handler/udp/UDPServerRequestHandler.java
+++ Kore/src/main/java/com/kore/handler/udp/UDPServerRequestHandler.java
@@ -1,4 +1,47 @@
 ﻿package com.kore.handler.udp;
 
-public class UDPServerRequestHandler {
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.kore.handler.interfaces.ServerRequestHandlerInterface;
+import com.kore.invoker.Invoker;
+
+public class TCPServerRequestHandler implements ServerRequestHandlerInterface {
+    private DatagramSocket serverSocket;
+
+    private final ExecutorService executorService;
+
+    public void start(int port) {
+        try {
+            this.serverSocket = new DatagramSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public UDPServerRequestHandler(int port, Invoker invoker){
+        start(port);
+
+        this.executorService = Executors.newCachedThreadPool();
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                byte[] receiveMessage = new byte[1024];
+				DatagramPacket receivePacket = new DatagramPacket(receiveMessage, receiveMessage.length);
+                executorService.execute(new UDPRequestHandler(receivePacket, invoker));
+            } catch (IOException e) {
+                if (serverSocket.isClosed()) {
+                    break;
+                }
+            }
+        }
+    }
+    
+    
 }
+
