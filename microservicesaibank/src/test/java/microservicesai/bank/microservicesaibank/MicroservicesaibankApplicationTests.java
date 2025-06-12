a7237e5a2242e87a12ff090a88f0f4f7118d990c
Luís Eduardo de Oliveira Sales <40070690+luisesales@users.noreply.github.com>
2025-06-12 14:18:41 +0000

    starting microservices app

---
+++ microservicesaibank/src/test/java/microservicesai/bank/microservicesaibank/MicroservicesaibankApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicesai.bank.microservicesaibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesaibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
