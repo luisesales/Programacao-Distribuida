d6109288d9701d901347968eeedb300df8c169bb
luisesales <duduosales@hotmail.com>
2025-07-01 11:28:35 -0300

    adding new requests in JMeter

--- Middleware/App/src/main/java/app/Main.java
+++ Middleware/App/src/main/java/app/Main.java
@@ -4,7 +4,6 @@ import com.kore.annotations.KoreApplication;
 
 @KoreApplication
 public class Main {
-
     public static void main(String[] args) {            
         com.kore.broker.KoreApplication.run(Main.class, args);        
     }
