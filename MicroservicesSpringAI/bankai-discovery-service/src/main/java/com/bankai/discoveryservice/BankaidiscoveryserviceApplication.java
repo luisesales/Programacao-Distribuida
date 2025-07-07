b722b6739ce7d611fb4001e122e98a149e4e8d18
luisesales <duduosales@hotmail.com>
2025-07-07 16:13:32 -0300

    attempt to make discovery service recognize config file

--- MicroservicesSpringAI/bankai-discovery-service/src/main/java/com/bankai/discoveryservice/BankaidiscoveryserviceApplication.java
+++ MicroservicesSpringAI/bankai-discovery-service/src/main/java/com/bankai/discoveryservice/BankaidiscoveryserviceApplication.java
@@ -2,11 +2,10 @@ package com.bankai.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication	
 public class BankaidiscoveryserviceApplication {
 
 	public static void main(String[] args) {
